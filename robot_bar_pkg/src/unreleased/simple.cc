37d0b88f882eb9d1ad8a8e5aef7c51e2db123a07
Kazuhiro Sasabuchi <ssb@dhcp150.jsk.t.u-tokyo.ac.jp>
2014-07-10 17:21:50 +0900

    もうしばらくコーディングいいや

--- robot_bar_pkg/src/unreleased/simple.cc
+++ robot_bar_pkg/src/unreleased/simple.cc
@@ -2,10 +2,30 @@
 
 namespace simple {
 
+void simple::LoadScenario() {
+  std::ifstream file(filename_.c_str());
+  std::string text;
+  file >> size_;
+  table_.resize(size_);
+  for (int i=0; i<size_; ++i) {
+    std::string text;
+    file >> text;
+    char* text_c = new char[text.size()+1];
+    std::copy(text.begin(), text.end(), text_c);
+    text_c[text.size()] = '\0';
+    table_[i].key = std::string(strtok(text_c, ","));
+    table_[i].output.data = std::string(strtok(NULL, "\n"));
+    delete text_c;
+  }
+  file.close();
+}
+
 void simple::Callback(const std_msgs::String::ConstPtr& input) {
   for (int i=0; i<size_; ++i)
-    if (input->data == table_[i].key)
+    if (strstr(input->data.c_str(), table_[i].key.c_str()) != NULL) {
       pub_.publish(table_[i].output);
+      return;
+    }
 }
 
 }
@@ -14,6 +34,7 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "test");
   ros::NodeHandle nh;
   simple::simple test(nh);
+  test.LoadScenario();
   while (ros::ok()) {
     ros::spinOnce();
   }
--- robot_bar_pkg/src/unreleased/simple.h
+++ robot_bar_pkg/src/unreleased/simple.h
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <fstream>
 
 namespace simple {
 
@@ -13,10 +14,13 @@ class simple {
   explicit simple(ros::NodeHandle nh) {
     pub_ = nh.advertise<std_msgs::String>("output", 1000);
     sub_ = nh.subscribe("input", 100, &simple::Callback, this);
+    nh.getParam("filename", filename_);
   }
   virtual ~simple() {};
+  void LoadScenario();
  private:
   int size_;
+  std::string filename_;
   ros::Publisher pub_;
   ros::Subscriber sub_;
   std::vector<table> table_;
