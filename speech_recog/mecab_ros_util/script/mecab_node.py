bfb055842c7517ba8434672a9171940558663557
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-07-20 19:50:26 +0900

    add sample mecab_node impl

---
+++ speech_recog/mecab_ros_util/script/mecab_node.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import MeCab
+import sys
+import string
+
+import rospy
+import json
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+from std_msgs.msg import Empty
+
+res_pub = ""
+mecab_tagger = MeCab.Tagger (" ".join(sys.argv))
+
+def request_strint_callback(data):
+  res_pub.publish(String(mecab_tagger.parse(data.data)))
+
+if __name__ == '__main__':
+  rospy.init_node('mecab_node', anonymous=True)
+  res_pub = rospy.Publisher('mecab_node/response/string', String)
+  rospy.Subscriber("mecab_node/request/string", String, request_strint_callback)
+  rospy.spin()
