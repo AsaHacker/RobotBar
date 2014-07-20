bf21e9a453007319ed05e0331cbf24452796e639
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-07-20 19:50:41 +0900

    add some dif

--- speech_recog/mecab_ros_util/install.sh
+++ speech_recog/mecab_ros_util/install.sh
@@ -1,10 +1,12 @@
-START_DIR=`pwd` ;
-MECAB_ROS_ROOT=`rospack find mecab_ros_util`/3rdparty ;
-TARGET_MECAB_PATH=mecab-python-0.996.tar.gz ;
-MECAB_ROOT_PATH=mecab ;
-cd $MECAB_ROS_ROOT ;
-wget https://mecab.googlecode.com/files/$TARGET_MECAB_PATH ;
-tar zxvf $TARGET_MECAB_PATH ;
-mv $TARGET_MECAB_PATH $MECAB_ROOT_PATH ;
-cd $START_DIR ;
+# START_DIR=`pwd` ;
+# MECAB_ROS_ROOT=`rospack find mecab_ros_util`/3rdparty ;
+# TARGET_MECAB_PATH=mecab-python-0.996 ;
+# MECAB_ROOT_PATH=mecab ;
+# cd $MECAB_ROS_ROOT ;
+# wget https://mecab.googlecode.com/files/$TARGET_MECAB_PATH.tar.gz ;
+# tar zxvf $TARGET_MECAB_PATH.tar.gz ;
+# mv $TARGET_MECAB_PATH $MECAB_ROOT_PATH ;
+# cd $START_DIR ;
 
+sudo apt-get install libmecab-dev libmecab1 mecab mecab-jumandic mecab-utils mecab-ipadic mecab-ipadic-utf8 python-mecab ipadic ;
+## please change configuration of pythonpath correctly.
