ca46050c41974036053bc0144c239b279bbd39f0
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-07-25 02:20:03 +0900

    add check replies functino

--- twitter/twitter_ros_util/script/tweet.py
+++ twitter/twitter_ros_util/script/tweet.py
@@ -21,6 +21,20 @@ def tweet(data):
         print "postupdate " + message + "(" + filename + ")"
         api.PostUpdate(message)
 
+def check_replies():
+    global prev_reply_ids
+    buf = api.GetReplies()
+    if prev_reply_ids:
+        for rep in buf:
+            if rep.user.screen_name:
+                print rep.user.screen_name + " says " + rep.text
+                if not rep.id in prev_reply_ids and key["username"] in rep.text:
+                    print "pub"
+                    rep_pub.publish(String(rep.in_reply_to_screen_name + "\n" + rep.text))
+    else:
+        print "empty"
+    prev_reply_ids = map(lambda x:x.id, buf)
+
 if __name__ == '__main__':
     rospy.init_node('rostwitter', anonymous=True)
     if os.environ.get("TWITTER_CONF_PATH"):
@@ -32,5 +46,10 @@ if __name__ == '__main__':
                       consumer_secret=key["api_secret"],
                       access_token_key=key["access_key"],
                       access_token_secret=key["access_secret"])
+    prev_reply_ids = []
+    rep_pub = rospy.Publisher('twitter/reply/string', String)
     rospy.Subscriber("robotbar/tweet/string", String, tweet)
-    rospy.spin()
+    ## rospy.spin()
+    while(not rospy.is_shutdown()):
+        check_replies()
+        rospy.sleep(30)
