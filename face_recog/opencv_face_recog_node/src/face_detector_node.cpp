33e67704a3603cfb34908a25230259b3352eac09
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-02-02 13:07:43 +0900

    able to set window flag usign environment value

--- face_recog/opencv_face_recog_node/src/face_detector_node.cpp
+++ face_recog/opencv_face_recog_node/src/face_detector_node.cpp
@@ -22,26 +22,29 @@ class FaceDetectorNode
 
   std::vector<cv::Rect> faces;
   cv::CascadeClassifier cascade;
+  int window_flag;
 
 public:
-  FaceDetectorNode() : it_(nh_)
+  FaceDetectorNode(int wf) : it_(nh_)
   {
-    image_sub_ = it_.subscribe("/face_detector/image/raw", 1,
-			       &FaceDetectorNode::imageCb, this);
-    compressed_image_sub_ = it_.subscribe("/face_detector/image", 1,
-					  &FaceDetectorNode::imageCb, this,
-					  image_transport::TransportHints("compressed")) ;
-    rect_pub = nh_.advertise<std_msgs::Float32MultiArray>("/face_detector/rect", 1);
-    normalized_rect_pub = nh_.advertise<std_msgs::Float32MultiArray>("/face_detector/normalized_rect", 1);
+    this->nh_ = nh_;
+    this->image_sub_ = it_.subscribe("/face_detector/image/raw", 1,
+				     &FaceDetectorNode::imageCb, this);
+    this->compressed_image_sub_ = it_.subscribe("/face_detector/image", 1,
+						&FaceDetectorNode::imageCb, this,
+						image_transport::TransportHints("compressed"));
+    this->rect_pub = this->nh_.advertise<std_msgs::Float32MultiArray>("/face_detector/rect", 1);
+    this->normalized_rect_pub = this->nh_.advertise<std_msgs::Float32MultiArray>("/face_detector/normalized_rect", 1);
 
     if(!cascade.load("/opt/ros/hydro/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml")) std::cout << "xml invalid" << std::endl ;
+    this->window_flag = wf;
 
-    cv::namedWindow(OPENCV_WINDOW);
+    if ( this->window_flag ) cv::namedWindow(OPENCV_WINDOW);
   }
 
   ~FaceDetectorNode()
   {
-    cv::destroyWindow(OPENCV_WINDOW);
+    if ( this->window_flag ) cv::destroyWindow(OPENCV_WINDOW);
   }
 
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
@@ -62,38 +65,41 @@ public:
     //cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
     // tarImg = cv_ptr->image ;
     // face = cvHaarDetectObjects(tarImg, cvHCC, cvMStr);
-    cascade.detectMultiScale(cv_ptr->image, faces,
-			     1.3,2,CV_HAAR_SCALE_IMAGE, cv::Size(50, 50));
+    this->cascade.detectMultiScale(cv_ptr->image, this->faces,
+				   1.3,2,CV_HAAR_SCALE_IMAGE, cv::Size(50, 50));
     cv::Mat image_mat = cv_ptr->image ;
-    std::cout << faces.size() << " face detected" << std::endl ;
-    for (int i = 0; i < faces.size() ; i++) {
-      cv::Rect faceRect = faces.at(i) ;
+    std::cout << this->faces.size() << " face detected" << std::endl ;
+    for (int i = 0; i < this->faces.size() ; i++) {
+      cv::Rect faceRect = this->faces.at(i) ;
       cv::Point left_top_p, right_bot_p ;
       left_top_p.x = faceRect.x ;
       left_top_p.y = faceRect.y ;
       right_bot_p.x = faceRect.x + faceRect.width ;
       right_bot_p.y = faceRect.y + faceRect.height ;
-      cv::rectangle(cv_ptr->image, left_top_p, right_bot_p,
-		    CV_RGB(255, 0 ,0), 3, CV_AA);
+      if ( this->window_flag )
+	cv::rectangle(cv_ptr->image, left_top_p, right_bot_p,
+		      CV_RGB(255, 0 ,0), 3, CV_AA);
       std_msgs::Float32MultiArray pub_msg ;
       std::vector<float> rect(4) ;
       rect[0] = faceRect.x; rect[1] = faceRect.y;
       rect[2] = faceRect.width; rect[3] = faceRect.height;
       pub_msg.data = rect ;
-      rect_pub.publish(pub_msg) ;
+      this->rect_pub.publish(pub_msg) ;
 
       rect[0] /= image_mat.cols ;
       rect[2] /= image_mat.cols ;
       rect[1] /= image_mat.rows ;
       rect[3] /= image_mat.rows ;
       pub_msg.data = rect ;
-      normalized_rect_pub.publish(pub_msg) ;
+      this->normalized_rect_pub.publish(pub_msg) ;
     }
 
 
     // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
+    if ( this->window_flag ) {
+      cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+      cv::waitKey(3);
+    }
 
     // Output modified video stream
     //image_pub_.publish(cv_ptr->toImageMsg());
@@ -104,7 +110,11 @@ public:
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "image_converter");
-  FaceDetectorNode ic;
+  int wf = 1;
+  if ( const char* wfs = std::getenv("FACE_DETECT_NODE_WINDOW_FLAG") ){
+    wf = atoi(wfs);
+  }
+  FaceDetectorNode ic(wf);
   ros::spin();
   return 0;
 }
